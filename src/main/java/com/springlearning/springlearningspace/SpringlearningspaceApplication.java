2fd298c82104ae3ece84eafacb09d77d9c9e1b72
Bharat kumar Dumpa <bharat@Bharats-MBP.attlocal.net>
2023-11-02 22:19:58 -0500

    started different git learnings

---
+++ src/main/java/com/springlearning/springlearningspace/SpringlearningspaceApplication.java
@@ -0,0 +1,16 @@
+package com.springlearning.springlearningspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringlearningspaceApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringlearningspaceApplication.class, args);
+
+		System.out.println("here");
+	}
+
+}
---
+++ src/test/java/com/springlearning/springlearningspace/SpringlearningspaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springlearning.springlearningspace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringlearningspaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
